ee5cc5271b200f4051eca37f1e7d28d9c64da487
Monali9976 <114649777+Monali9976@users.noreply.github.com>
2025-07-01 15:55:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,4 +17,6 @@ app.use("/api/chats", require("./routes/chat"));
 
 app.use("/api/chat", chatRoute);
 app.use("/admin/chats", chatRoute); // optional
-app.listen(5000, () => console.log("Server running on port 5000"));
+// app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
